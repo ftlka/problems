a40b0a0220a8ad7f3c3a501c589315f46c1a57bd
ftlka <smart.Julia@yandex.ru>
2019-03-13 00:13:21 +0500

    min-cost-climbing-stairs

---
+++ leetcode/min-cost-climbing-stairs/solution.py
@@ -0,0 +1,11 @@
+from math import inf
+
+
+def minCostClimbingStairs(cost):
+	dp = [inf] * len(cost)
+	dp[0], dp[1] = cost[0], cost[1]
+
+	for i in range(2, len(cost)):
+		dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+
+	return min(dp[-1], dp[-2])
---
+++ leetcode/min-cost-climbing-stairs/test_.py
@@ -0,0 +1,13 @@
+from solution import minCostClimbingStairs
+
+assert minCostClimbingStairs([10, 15, 20]) == 15
+
+assert minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]) == 6
+
+assert minCostClimbingStairs([0, 0, 0]) == 0
+
+assert minCostClimbingStairs([1, 2]) == 1
+
+assert minCostClimbingStairs([2, 1]) == 1
+
+assert minCostClimbingStairs([1, 1]) == 1
